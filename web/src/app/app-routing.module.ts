53110fbd4fec18679fc7f5050c3f914f399790ca
Steven Imhof <steven.imhof@raiffeisen.ch>
2019-05-09 13:43:45 +0200

    added auth guard

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,7 +10,8 @@ const routes: Routes = [
   },
   {
     path: 'toeggeli',
-    loadChildren: './toeggeli/toeggeli.module#ToeggeliModule'
+    loadChildren: './toeggeli/toeggeli.module#ToeggeliModule',
+    canActivate: [AuthGuard]
   },
   {
     path: 'auth',
