f7063df76e850a6c9de50b1cdb206cbc0aa5ca13
Dominik Forster <dominik.forster@raiffeisen.ch>
2019-05-10 08:23:51 +0200

    function eu west

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,6 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-
-    response.send("Hello from Toeggeli!");
-
+export const helloWorld = functions.region('europe-west1').https.onRequest((request, response) => {
+  response.send('Hello from Toeggeli!');
 });
