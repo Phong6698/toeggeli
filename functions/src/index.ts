e169e7c5214f77af9cd221294f836a42078296b4
Chiramet Phong Penglerd <Phong@hotmail.ch>
2019-05-31 16:17:14 +0200

    cloud function updateStatistics cleanup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,6 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp(functions.config().firebase);
 
-
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
@@ -11,29 +10,29 @@ export const helloWorld = functions.region('europe-west1').https.onRequest((requ
     response.send('Hello from Toeggeli!');
 });
 
-
 export const updateStatistics = functions.region('europe-west1')
-    .firestore.document('Matches/{matchID}').onCreate((snap, context) => {
-
+    .firestore.document('Matches/{matchID}').onCreate((snap) => {
         const createdMatch: any = snap.data();
-        createdMatch.players.forEach((player: any) => {
 
-            const didPlayerWon = (player.side === 'blue' && createdMatch.blueScore === 10) ||
-                (player.side === 'red' && createdMatch.redScore === 10);
+        // Transactional
+        return admin.firestore().runTransaction(async transaction => {
+            try {
+                const promises: Promise<any>[] = [];
+                createdMatch.players.forEach((player: any) => {
+                    const statisticQuery = admin.firestore().collection('Statistics')
+                        .where('userID', '==', player.userID)
+                        .where('spaceID', '==', createdMatch.spaceID);
+                    promises.push(transaction.get(statisticQuery));
+                });
 
-            const statisticQuery = admin.firestore().collection('Statistics')
-                .where('userID', '==', player.userID)
-                .where('spaceID', '==', createdMatch.spaceID);
-
-            // Transactionnal
-            return admin.firestore().runTransaction(transaction => {
-
-                return transaction.get(statisticQuery).then(docSnap => {
-
+                const docSnaps = await Promise.all(promises);
+                docSnaps.forEach((docSnap, index) => {
+                    const didPlayerWon = (createdMatch.players[index].side === 'blue' && createdMatch.blueScore === 10) ||
+                        (createdMatch.players[index].side === 'red' && createdMatch.redScore === 10);
                     if (!docSnap.empty) {
                         const statisticDoc = docSnap.docs[0];
-                        const updateData = didPlayerWon ? { wins: statisticDoc.data().wins + 1 } : { losses: statisticDoc.data().losses + 1 };
-                        return transaction.update(statisticDoc.ref, updateData);
+                        const updateData = didPlayerWon ? {wins: statisticDoc.data().wins + 1} : {losses: statisticDoc.data().losses + 1};
+                        transaction.update(statisticDoc.ref, updateData);
                     } else {
                         let wins = 0;
                         let losses = 0;
@@ -42,20 +41,18 @@ export const updateStatistics = functions.region('europe-west1')
                         } else {
                             losses = 1;
                         }
-                        const newStatsistics = {
+                        const newStatistics = {
                             wins: wins,
                             losses: losses,
                             elo: 0,
                             spaceID: createdMatch.spaceID,
-                            userID: player.userID
+                            userID: createdMatch.players[index].userID
                         };
-                        return transaction.set(admin.firestore().collection('Statistics').doc(), newStatsistics);
+                        transaction.set(admin.firestore().collection('Statistics').doc(), newStatistics);
                     }
-
-                }).catch(() => console.error('Could not get Statistics'));
-            });
+                });
+            } catch (error) {
+                return error;
+            }
         });
-
-        return true;
-
     });
