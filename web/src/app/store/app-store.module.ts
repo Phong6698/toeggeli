c9b6c5a1d393db9beef92efe4a7e37475127959b
Chiramet (Phong) Penglerd <Phong@hotmail.ch>
2019-06-07 13:18:55 +0200

    ng update @ngrx/store

--- web/src/app/store/app-store.module.ts
+++ web/src/app/store/app-store.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { EffectsModule } from '@ngrx/effects';
-import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../../environments/environment';
-import { metaReducers, reducers } from './app-store.reducer';
-import { AuthEffects } from './auth.effects';
-import { CustomRouterStateSerializer } from './custom-router-store-serializer';
+import {NgModule} from '@angular/core';
+import {EffectsModule} from '@ngrx/effects';
+import {RouterStateSerializer, StoreRouterConnectingModule} from '@ngrx/router-store';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../../environments/environment';
+import {metaReducers, reducers} from './app-store.reducer';
+import {AuthEffects} from './auth.effects';
+import {CustomRouterStateSerializer} from './custom-router-store-serializer';
 
 @NgModule({
   declarations: [],
   imports: [
-    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreModule.forRoot(reducers, { metaReducers, runtimeChecks: { strictStateImmutability: true, strictActionImmutability: true } }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
     EffectsModule.forRoot([AuthEffects])
--- web/src/app/store/app-store.reducer.ts
+++ web/src/app/store/app-store.reducer.ts
@@ -1,9 +1,9 @@
-import { routerReducer, RouterReducerState } from '@ngrx/router-store';
-import { ActionReducerMap, createSelector, MetaReducer } from '@ngrx/store';
-import { storeFreeze } from 'ngrx-store-freeze';
-import { environment } from '../../environments/environment';
-import { authReducer, AuthState } from './auth.reducer';
-import { RouterStateUrl } from './router-state-url';
+import {routerReducer, RouterReducerState} from '@ngrx/router-store';
+import {ActionReducerMap, createSelector, MetaReducer} from '@ngrx/store';
+
+import {environment} from '../../environments/environment';
+import {authReducer, AuthState} from './auth.reducer';
+import {RouterStateUrl} from './router-state-url';
 
 export interface AppState {
   router: RouterReducerState<RouterStateUrl>;
@@ -15,7 +15,7 @@ export const reducers: ActionReducerMap<AppState> = {
   auth: authReducer
 };
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
 
 export const selectRouter = (state: AppState) => state.router;
 
