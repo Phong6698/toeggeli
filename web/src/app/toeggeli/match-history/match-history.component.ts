79c870d2d95e1c020f541e3c0ed43e2507a8be5e
upr7872 <ueli.kistler@raiffeisen.ch>
2019-11-12 13:15:01 +0100

    Match History

--- web/src/app/toeggeli/match-history/match-history.component.ts
+++ web/src/app/toeggeli/match-history/match-history.component.ts
@@ -3,9 +3,10 @@ import {Store} from '@ngrx/store';
 import {AppState} from '../../store/app-store.reducer';
 import {Observable, Subject} from 'rxjs';
 import {Match} from '../../core/match.service';
-import {selectMatchHistory, selectToeggeliSelectedSpaceId} from '../toeggeli.reducer';
+import {selectMatchHistory, selectSpaceUsers, selectToeggeliSelectedSpaceId} from '../toeggeli.reducer';
 import {matchHistoryRequested} from '../toeggeli.actions';
 import {takeUntil} from 'rxjs/operators';
+import {User} from '../user';
 
 @Component({
   selector: 'app-match-history',
@@ -17,6 +18,7 @@ export class MatchHistoryComponent implements OnInit, OnDestroy {
   destroy$ = new Subject<void>();
 
   matches$: Observable<Match[]>;
+  users: User[];
 
   constructor(private store: Store<AppState>) { }
 
@@ -27,11 +29,18 @@ export class MatchHistoryComponent implements OnInit, OnDestroy {
       }
     });
     this.matches$ = this.store.select(selectMatchHistory);
+    this.store.select(selectSpaceUsers).pipe(takeUntil(this.destroy$))
+      .subscribe(users => this.users = users);
   }
 
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  getPlayerFromMatch(players: any[], team: 'blue' | 'red', index: 0|1): User {
+    const userID = players.filter(player => player.side === team)[index].userID;
+    return this.users.find(user => user.id === userID);
   }
 
 }
